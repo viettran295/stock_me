ac9346c0047b1ca8341d544017c4545bbeba3f38
viettr <viettran295@gmail.com>
2024-05-18 21:52:07 +0200

    Visulize asset structure

--- stock_me/balance_sheet.py
+++ stock_me/balance_sheet.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 from .stock_me import StockMe
+from datetime import datetime as dt
+import plotly.express as px
 
 class BalanceSheet(StockMe):
     def __init__(self) -> None:
@@ -10,3 +12,11 @@ class BalanceSheet(StockMe):
 
         self.asset_structure = ["Total Liabilities Net Minority Interest", 
                                 "Total Equity Gross Minority Interest"]
+        
+    def show_asset_structure(self, df: pd.DataFrame):
+        years = 4
+        df = pd.melt(df, id_vars=[self.idx_column], value_vars=[f"{dt.now().year - i}" for i in range(years)],
+                    var_name="Year", value_name="Dollars")
+        fig = px.bar(df, x=df["Year"], y=df["Dollars"], color=df[self.idx_column],
+                    title="Asset Structure", width=800)
+        fig.show()
