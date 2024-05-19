0ca8cf680c9acccb5467dd0b15b2cbe717334f8e
viettr <viettran295@gmail.com>
2024-05-19 14:32:21 +0200

    Update current year and years to analyze as attribute

--- stock_me/stock_me.py
+++ stock_me/stock_me.py
@@ -1,21 +1,21 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime as dt
 
 class StockMe:
     def __init__(self) -> None:
         self.idx_column = "Criteria"
+        self.currYear = dt.now().year
+        self.analyze_years = 4
 
     def pick_criteria(self, df: pd.DataFrame, criteria) -> pd.DataFrame:
-        df = self.rename_reset_Idx(df, self.idx_column)
+        df = self.rename_reset_idx(df, self.idx_column)
         return df.loc[df[self.idx_column].isin(criteria)]
     
-    @staticmethod
-    def rename_reset_Idx(df: pd.DataFrame, name: str) -> pd.DataFrame:
-        current_year = datetime.now().year
+    def rename_reset_idx(self, df: pd.DataFrame, name: str) -> pd.DataFrame:
         years = 4
         cols_year = []
         for i in range(years):
-             cols_year.append(f"{current_year - i}")
+             cols_year.append(f"{self.currYear - i}")
         df = df.set_axis(cols_year, axis="columns")
         df.reset_index(inplace=True)
         return df.rename(columns={'index': name})
