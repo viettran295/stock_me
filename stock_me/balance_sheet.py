eb63403f9018ec4156b381aea2223283a6709624
viettr <viettran295@gmail.com>
2024-05-19 12:47:15 +0200

    Update assets structure

--- stock_me/balance_sheet.py
+++ stock_me/balance_sheet.py
@@ -6,15 +6,17 @@ import plotly.express as px
 class BalanceSheet(StockMe):
     def __init__(self) -> None:
         super().__init__()
-        self.asset = ["Current Assets", "Cash And Cash Equivalents", "Receivables", 
-                      "Total Non Current Assets", "Net PPE", "Goodwill And Other Intangible Assets", 
-                      "Investments And Advances"]
+                        # Current assets
+        self.asset = ["Cash And Cash Equivalents", "Receivables", "Prepaid Assets", "Other Current Assets", "Restricted Cash", 
+                        # Non-current assets
+                      "Net PPE", "Goodwill And Other Intangible Assets", "Investments And Advances", "Non Current Deferred Assets", "Non Current Prepaid Assets", "Other Non Current Assets"]
 
         self.asset_structure = ["Total Liabilities Net Minority Interest", 
                                 "Total Equity Gross Minority Interest"]
         
     def show_asset_structure(self, df: pd.DataFrame):
         years = 4
+        df = self.pick_criteria(df, self.asset)
         df = pd.melt(df, id_vars=[self.idx_column], value_vars=[f"{dt.now().year - i}" for i in range(years)],
                     var_name="Year", value_name="Dollars")
         fig = px.bar(df, x=df["Year"], y=df["Dollars"], color=df[self.idx_column],
