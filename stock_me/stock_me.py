95d61d6b8df6cc1fa17ce435c3b5aa5de2e87933
viettr <viettran295@gmail.com>
2024-05-28 21:33:00 +0200

    Fix: analyze years s depend on columns

--- stock_me/stock_me.py
+++ stock_me/stock_me.py
@@ -6,16 +6,16 @@ class StockMe:
     def __init__(self) -> None:
         self.idx_column = "Criteria"
         self.currYear = dt.now().year
-        self.analyze_years = 4
+        self.analyze_years = 0
 
     def pick_criteria(self, df: pd.DataFrame, criteria) -> pl.DataFrame:
         df = pl.from_pandas(self.rename_reset_idx(df, self.idx_column))
         return df.filter(pl.col(f'{self.idx_column}').is_in(criteria))
 
     def rename_reset_idx(self, df: pd.DataFrame, name: str) -> pd.DataFrame:
-        years = 4
         cols_year = []
-        for i in range(years):
+        self.analyze_years = len(df.columns)
+        for i in range(self.analyze_years):
              cols_year.append(f"{self.currYear - i}")
         df = df.set_axis(cols_year, axis="columns")
         df.reset_index(inplace=True)
