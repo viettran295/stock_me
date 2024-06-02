8a03dff466ed4e635bd6e65efbe8d51cf5c65565
viettr <viettran295@gmail.com>
2024-06-02 21:15:16 +0200

    Fix axis name

--- stock_me/balance_sheet.py
+++ stock_me/balance_sheet.py
@@ -20,7 +20,7 @@ class BalanceSheet(StockMe):
         df = self.pick_criteria(df, self.asset_structure)
         df = df.melt(id_vars=[self.idx_column], value_vars=[f"{dt.now().year - i}" for i in range(self.analyze_years)],
                     variable_name="Year", value_name="Dollars")
-        fig = px.bar(x=df["Year"], y=df["Dollars"], color=df[self.idx_column], template="plotly_dark",
+        fig = px.bar(df, x="Year", y="Dollars", color=df[self.idx_column], template="plotly_dark",
                     title="Asset Structure", width=800)
         return fig
     
--- stock_me/cashflow.py
+++ stock_me/cashflow.py
@@ -13,6 +13,6 @@ class CashFlow(StockMe):
         df = self.pick_criteria(df, self.cash_flow)
         df = df.melt(id_vars=[self.idx_column], value_vars=[f"{dt.now().year - i}" for i in range(self.analyze_years)],
                     variable_name="Year", value_name="Dollars")
-        fig = px.bar(x=df["Year"], y=df["Dollars"], color=df[self.idx_column], template="plotly_dark",
+        fig = px.bar(df, x="Year", y="Dollars", color=df[self.idx_column], template="plotly_dark",
                     title="Cash flow", width=800)
         return fig
